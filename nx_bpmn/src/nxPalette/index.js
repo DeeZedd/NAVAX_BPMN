5f1a8bd4f94b632d8ef146f3c9e1eca38fcf8ab7
Daniel Zangerl <danielzangerl@Air-von-Daniel.lan>
2022-05-27 13:45:00 +0200

    removed unused components

--- nx_bpmn/src/nxPalette/index.js
+++
@@ -1,8 +0,0 @@
-import nxContextPad from "./nxContextPad";
-import nxPalette from "./nxPalette";
-
-export default {
-    __init__: [ 'nxContextPad', 'nxPalette' ],
-    nxContextPad: [ 'type', nxContextPad ],
-    nxPalette: [ 'type', nxPalette ]
-};
--- nx_bpmn/src/overrides/overridePalette.js
+++
@@ -1,9 +0,0 @@
-export default function CustomPaletteProvider(palette) {
-    palette.registerProvider(this);
-
-    this.getPaletteEntries = function(element) {
-        return {};
-    };
-}
-
-CustomPaletteProvider.$inject = [ 'palette' ];
